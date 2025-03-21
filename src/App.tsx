fdf425164e89830024fdefedd88d5686a54d2c90
Francis Maina <106233737+Maina-Francis@users.noreply.github.com>
2025-03-21 08:44:05 +0000

    Reinitialize project structure
    This commit reinitializes the project structure with the specified technologies, removing all existing components and configurations.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Journal from "./pages/Journal";
-import Insights from "./pages/Insights";
-import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
 
 const queryClient = new QueryClient();
 
@@ -18,11 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/journal" element={<Journal />} />
-          <Route path="/insights" element={<Insights />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
