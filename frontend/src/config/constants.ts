4e7e686fe3b43f6d7ed00558f41f254e1b6793f4
Francis Maina <francismainakabutha@gmail.com>
2025-04-02 08:36:07 +0300

    fix: adds correct backend url

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,4 +1,4 @@
 
 export const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://shamiri-journal-backend.onrender.com/api/test' 
+  ? 'https://shamiri-journal-backend.onrender.com/api' 
   : 'http://localhost:5000/api';
